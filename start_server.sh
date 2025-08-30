e5298e9e35649966bea60c85b0a64c4fb709d51d
rogercc <roger.creus-castanyer@mila.quebec>
2025-08-30 16:12:57 -0400

    adding sbatch

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source /opt/conda/etc/profile.d/conda.sh
 conda activate pokeagent
-tmux new-session -d -s server "python -m server.app"
-echo "Server started in tmux session 'server'. Attach with: tmux attach -t server"
+tmux new-session -A -s server "python -m server.app"
 exec bash
